eaadd8bd50b3cc1e992e72ab069654b7f66da04a
Joshua C <joshuat2000@outlook.com>
2021-03-03 19:32:50 +0000

    Battle now advances

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -2,36 +2,185 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum BattleStates { START, ENEMYTURN, PLAYERTURN, WIN, LOSE }
-
 public class StateMachine : MonoBehaviour
 {
-    public BattleStates states;
+    //editor vars
+    public GameObject enemyPrefab;
 
-    // Start is called before the first frame update
+    public Transform enemyLocation;
+
+    //script references
+    UpdateUI updateUI;
+    EnemyUnit enemyUnit;
+
+    //system vars
+    int questionNumber = 1;
+    int battleScore, correctQuestions;
+    List<int> incorrectQuestions;
+    public bool incorrectLoop = false;
+
+    void Awake()
+    {
+        updateUI = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<UpdateUI>();
+
+        GameObject enemyGo = Instantiate(enemyPrefab, enemyLocation);
+        enemyUnit = enemyGo.GetComponent<EnemyUnit>();
+    }
+
     void Start()
     {
-        states = BattleStates.START;
+        incorrectQuestions = new List<int>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// calculates the players score and cycles through arrays in the incorrect loop
+    /// </summary>
+    /// <param name="result"></param>
+    void UpdateScore(bool result)
     {
-        
+        // if the player answered correctly
+        if (result)
+        {
+            //things that happen in both loops
+            correctQuestions++;
+            battleScore = correctQuestions * 200;
+            updateUI.DisplayResult("That answer was... correct!");
+            enemyUnit.TakeDamage(1);
+
+            //things that happen in incorrect loop
+            if (incorrectLoop)
+            {
+                enemyUnit.incorrectlyAnsweredQs.RemoveAt(1);
+            }
+        }
+        //if the player answered incorrectly
+        else
+        {
+            //things that happen in both loops 
+            updateUI.DisplayResult("That answer was... incorrect");
+            
+            //things that happen in incorrect loop
+            if (incorrectLoop)
+            {
+                // removes from list and adds to another as the system uses the second slot for calculations
+                // but the incorrect questions still need to be stored somewhere
+                this.incorrectQuestions.Add(enemyUnit.incorrectlyAnsweredQs[1]);
+                enemyUnit.incorrectlyAnsweredQs.RemoveAt(1);
+            }
+
+            //things that happen in first loop
+            else
+            {
+                enemyUnit.incorrectlyAnsweredQs.Add(questionNumber);
+            }
+        }
+
+        UpdateQuestion();
     }
 
+    /// <summary>
+    /// calculates the parameters to update the question on display
+    /// </summary>
+    void UpdateQuestion()
+    {
+        // checks to see if the questions are over and if there are any incorrect questions
+        // if true, enables the "incorrect loop"
+        if (enemyUnit.incorrectlyAnsweredQs.Count > 1 && 
+            questionNumber == enemyUnit.questions.Length)
+        {
+            incorrectLoop = true;
+        }
+
+        // checks if it should calculate the question on display
+        // firstly checks if the question isn't at the end of the array of questions yet
+        // or checks if the variable "incorrect loop" is true
+        if (questionNumber < enemyUnit.questions.Length || incorrectLoop)
+        {
+            //if its not the "incorrect loop", simply adds 1 to question counter
+            //also ensures chance2 is disabled
+            if (!incorrectLoop)
+            {
+                updateUI.DisplayChance2();
+                questionNumber++;
+            }
+            else
+            {
+                //ensures chance 2 is enabled
+                updateUI.DisplayChance2(true);
+                // if the incorrectly answered lists length is equal to 1 (it has a palceholder value to start)
+                // then it will end the battle
+                if (enemyUnit.incorrectlyAnsweredQs.Count == 1)
+                {
+                    EndBattle();
+                }
+                // otherwise, it sets the question number to be the second number in the list of this array
+                else
+                {
+                    questionNumber = enemyUnit.incorrectlyAnsweredQs[1];
+                }
+            }
+
+            //passes it onto the updateUI class with question number in toe
+            updateUI.DisplayQuestion(questionNumber);
+        }
+        else
+        {
+            EndBattle();
+        }
+    }
+
+    /// <summary>
+    /// sets up parameters to call the interval screen
+    /// </summary>
+    void EndBattle()
+    {
+        int aoIncorrectQuestions = this.incorrectQuestions.Count;
+
+        updateUI.DisplayResult("Quiz is over");
+
+        Debug.LogWarning("End Application - Cause: Questions Ended!");
+        return;
+    }
+
+    /// <summary>
+    /// updates the enemy on display
+    /// // currently unused
+    /// </summary>
+    public void UpdateEnemy()
+    {
+        if (updateUI.CheckForNewEnemy())
+        {
+            enemyUnit = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyUnit>();
+        }
+    }
+
+    /// <summary>
+    /// called when Button1 is pressed
+    /// </summary>
     public void OnButton1()
     {
+        Debug.Log("Button1 - " + updateUI.option1.text + " - pressed");
 
+        UpdateScore(enemyUnit.checkCorrectAnswer(questionNumber, updateUI.option1.text));
     }
 
+    /// <summary>
+    /// called when Button2 is pressed
+    /// </summary>
     public void OnButton2()
     {
+        Debug.Log("Button1 - " + updateUI.option2.text + " - pressed");
 
+        UpdateScore(enemyUnit.checkCorrectAnswer(questionNumber, updateUI.option2.text));
     }
 
+    /// <summary>
+    /// called when Button3 is pressed
+    /// </summary>
     public void OnButton3()
     {
+        Debug.Log("Button1 - " + updateUI.option3.text + " - pressed");
 
+        UpdateScore(enemyUnit.checkCorrectAnswer(questionNumber, updateUI.option3.text));
     }
 }
--- Assets/Scripts/UpdateUI.cs
+++ Assets/Scripts/UpdateUI.cs
@@ -7,42 +7,139 @@ public class UpdateUI : MonoBehaviour
 {
     public Text enemyName;
     public Text enemyAttack;
+    public Image chance;
     public Text option1;
     public Text option2;
     public Text option3;
 
     GameObject[] hpCovers;
+    [HideInInspector] public int currentQuestion = 0;
+
     EnemyUnit _enemy;
 
-    // Start is called before the first frame update
     void Start()
     {
         hpCovers = GameObject.FindGameObjectsWithTag("EnemyHpCovers");
+
+        fullUpdate(1);
+    }
+
+    /// <summary>
+    /// fully updates the user interface
+    /// </summary>
+    /// <param name="questionNo"> the question number to be asked (actual) </param>
+    public void fullUpdate(int questionNo)
+    {
         _enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyUnit>();
 
-        //updating enemy name
-        enemyName.text = _enemy.enemyName;
+        DisplayName();
 
-        //Updating Health Bar
+        DisplayHealth();
+
+        DisplayQuestion(questionNo);
+        DisplayChance2();
+    }
+
+    #region Display Statements
+    /// <summary>
+    /// updates enemy name plate
+    /// </summary>
+    public void DisplayName()
+    {
+        enemyName.text = _enemy.enemyName;
+    }
+
+    /// <summary>
+    /// updates the enemy health bar
+    /// </summary>
+    public void DisplayHealth()
+    {
         for (int i = 0; i < hpCovers.Length; i++)
         {
-            //Debug.Log(hpCovers[i].name[10] - '0');
-            if ((hpCovers[i].name[10] - '0') <= _enemy.hp-1) //converts number in the name of the hpCover to an int then compares it to the hp value of the enemy
+            //converts number in the name of the hpCover to an int 
+            //then compares it to the hp value of the enemy
+            if ((hpCovers[i].name[10] - '0') <= _enemy.curHP - 1)
+            {
                 hpCovers[i].SetActive(false);
+            }
+            else
+            {
+                if (!hpCovers[i].activeSelf)
+                    hpCovers[i].SetActive(true);
+            }
         }
-
-        DisplayQuestion(1);
     }
-    
-    public void DisplayQuestion(int questionNo) //actual question number must be passed in as -1 is added to ensure it is in the enemy's questions[]
+
+    /// <summary>
+    /// updates the box on the left to the question passed in
+    /// </summary>
+    /// <param name="questionNo"> the question number to be asked (actual) </param>
+    public void DisplayQuestion(int questionNo) 
     {
         questionNo--;
+
         try { enemyAttack.text = _enemy.questions[questionNo]; }
         catch { Debug.LogError("INVALID QUESTION INPUTTED");  return; }
 
-        option1.text = _enemy.answerArrays[questionNo][0];
-        option2.text = _enemy.answerArrays[questionNo][1];
-        option3.text = _enemy.answerArrays[questionNo][2];
+        currentQuestion = questionNo;
+
+        DisplayButton(ref option1, 0);
+        DisplayButton(ref option2, 1);
+        DisplayButton(ref option3, 2);
+    }
+
+    /// <summary>
+    /// controls the enabled / disable of the ui box which informs the player if this is their first time answering a question
+    /// </summary>
+    /// <param name="enable"> the bool passed in </param>
+    public void DisplayChance2(bool enable = false)
+    {
+        chance.gameObject.SetActive(enable);
+    }
+
+    /// <summary>
+    /// Updates the display button, changing the text and colour when pressed
+    /// </summary>
+    /// <param name="option"> the button you wish to alter </param>
+    /// <param name="i"> the question (out of 3) you wish to display </param>
+    void DisplayButton(ref Text option, int i)
+    {
+        option.text = _enemy.answerArrays[currentQuestion][i];
+
+        ColorBlock cb = option.GetComponentInParent<Button>().colors;
+
+        if (_enemy.checkCorrectAnswer(currentQuestion+1, option.text))
+        {
+            cb.pressedColor = Color.green;
+        }
+        else
+        {
+            cb.pressedColor = Color.red;
+        }
+
+        option.GetComponentInParent<Button>().colors = cb;
+    }
+
+    /// <summary>
+    /// updates the box on the left to the text passed in
+    /// </summary>
+    /// <param name="text"> string of text that will be displayed </param>
+    public void DisplayResult(string text)
+    {
+        enemyAttack.text = text;
+    }
+    #endregion
+
+    /// <summary>
+    /// checks if there is a new enemy on the field
+    /// </summary>
+    /// <returns>true = enemy is different; false = no change</returns>
+    public bool CheckForNewEnemy()
+    {
+        if (GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyUnit>() != _enemy)
+            return true;
+        else
+            return false;
     }
 
 }
